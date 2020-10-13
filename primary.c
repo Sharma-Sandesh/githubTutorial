12f2ff5f9637345001b8ede71c70018edc4aaa50
Uncle_Iroh007 <sandesh.sharma2000@gmail.com>
2020-10-12 20:53:13 -0500

    primary added

---
+++ primary.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+	int a,b;
+	printf("\n\nGimme 2 numbers: ");
+	scanf("%d %d",&a,&b);
+	printf("\n\nSum : %d\nDiff: %d\nProduct: %d\n\n",a+b,a-b,a*b);
+
+
+	return 0;
+}
